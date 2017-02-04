3a152fd3441c0740b9ae16aac25361976645ad3d
BatRamirez <jose.ramirez@lumenup.com>
2017-02-04 16:44:02 -0600

    Add files via upload
    Files

---
+++ index.js
@@ -0,0 +1,3 @@
+// require('./src/index.es6.js');
+// require('./src/index.css');
+require('./src/index.scss');
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack'),
+    path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+module.exports = {
+    debug: true,
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.es6.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("style.css")
+    ]
+};
